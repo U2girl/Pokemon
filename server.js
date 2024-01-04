ccfaaaaaaceaf51e45f006d85ea923b359fff6cf
U2girl <elycias@yahoo.com>
2024-01-04 17:38:10 -0500

    connected to my database, can see json in the browser

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.json());
-
+const pokemonData= require("./models/pokemon.js");
 app.get('/', (req, res) => {
     
         res.send('Hello World');
